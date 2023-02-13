2a5602b24c948ece093e27d73a99f7f9c541e4c3
salemsifeddine <saiftony5@gmail.com>
2023-02-13 16:18:37 +0100

    main

--- control_access/src/components/management.js
+++ control_access/src/components/management.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
 import '../static/css/management.css'
@@ -5,15 +6,42 @@ import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import DeleteIcon from '@mui/icons-material/Delete';
 import StackedLineChartIcon from '@mui/icons-material/StackedLineChart';
 import imgs from '../static/images/body.jpg' 
+import Chart from 'chart.js/auto';
 
 
 
+
+const data = {
+  labels: [
+    'Red',
+    'Blue',
+    'Yellow'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [300, 50, 100],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+};
+
+const config = {
+  type: 'pie',
+  data: data,
+};
+
+
+const chart = new Chart(ctx,config)
 function Management() {
   return (
     <div className='managecont'>
       <div className="leftmanag">
         <div className='chartmanage'>
-
+        <canvas id="myChart"></canvas>
         </div>
         <div className='adduser'>
           <div className='addusertogym'>
